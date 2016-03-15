0ab30fe1a9ba779e24f93451054fec5a1c5ed00d
saxar11 <saxar11@gmail.com>
2016-03-15 22:14:49 +0200

    nickname generator stub

---
+++ src/main/java/ua/sakharevich/servlets/nickNameGen.java
@@ -0,0 +1,22 @@
+package ua.sakharevich.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Павел on 05.08.2015.
+ */
+@WebServlet("/nickNameGen")
+public class nickNameGen extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
